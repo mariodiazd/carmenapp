612d3caecc12cc965070692aebf29d36b7ed3514
mariodiazdiestro@gmail.com <mariodiazdiestro@gmail.com>
2022-07-16 16:20:23 +0200

    dsadsa

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
-
 const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/carmenapp'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/carmenapp/index.html'));});
+
+// PathLocationStrategy
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/carmenapp/index.html'));
+})
 app.listen(process.env.PORT || 8080);
